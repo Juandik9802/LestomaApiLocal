f7e4d5d7c1b85a245cfe29348c2009cb70b18b5b
JUAN DIEGO PACHON POVEDA <juandi240298@hotmail.com>
2023-04-24 11:00:01 -0500

    OK

---
+++ LestomaApiLocal/Controllers/Configuracion.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LestomaApiLocal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Configuracion : ControllerBase
+    {
+        // GET: api/<Configuracion>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<Configuracion>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Configuracion>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Configuracion>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Configuracion>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LestomaApiLocal/Controllers/Usuarios.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LestomaApiLocal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Usuarios : ControllerBase
+    {
+        // GET: api/<Usuarios>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        // Obtencion de datos de api de 
+        // GET api/<Usuarios>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Usuarios>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Usuarios>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Usuarios>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
