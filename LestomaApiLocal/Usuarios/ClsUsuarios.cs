f4ee6a465386abeb6917e0060ea659ce75784f53
JUAN DIEGO PACHON POVEDA <juandi240298@hotmail.com>
2023-05-18 10:19:10 -0500

    modificaciones

---
+++ LestomaApiLocal/Usuarios/ClsUsuarios.cs
@@ -0,0 +1,12 @@
+﻿namespace LestomaApiLocal.Usuarios
+{
+    public class ClsUsuarios
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+        public string ActividadesAsignadas { get; set; }
+    }
+}
